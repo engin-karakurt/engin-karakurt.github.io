299d39c8eaf8663ef6b2aacd2c0109b5b05d154b
engin-karakurt <155079545+engin-karakurt@users.noreply.github.com>
2024-06-25 04:05:30 +0200

    Remove slider & react, add project cards, remove unused pictures, optimize more images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://engin-karakurt.github.io",
-  integrations: [react(), tailwind()],
+  integrations: [tailwind()],
 });
